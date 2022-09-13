1d164e823ffd0af734c39beddd763d557258f3ad
Armen2552 <armenhayrapetyan@gmail.com>
2022-09-13 15:12:58 +0400

    13-09

--- consultary/src/components/Section-welcome/index.jsx
+++ consultary/src/components/Section-welcome/index.jsx
@@ -1,51 +1,67 @@
 import React, {useState} from "react";
 import "./style.scss"
-import Vector1 from "../../assets/images/Vector1.png"
-import Vector2 from "../../assets/images/Vector2.png"
-import Vector3 from "../../assets/images/Vector3.png"
-import Vector4 from "../../assets/images/Vector4.png"
+import Construction from "../../assets/images/construction.png"
+import Agricaltural from "../../assets/images/agriculture.png"
+import Catering from "../../assets/images/cathering.png"
+import Logistics from "../../assets/images/logistic.png"
+import Housekeeping from "../../assets/images/housekeeping.png"
+import Administration from "../../assets/images/adiministration.png"
+import Customer from "../../assets/images/customer.png"
 
 const Welcome = ()=>{
 
     const [box] = useState([
         {
-            img: Vector1,
-            path: '/about',
-            title: 'About ',
-            color: "#B5D56A",
+            img: Construction,
+            title: "Construction",
+            number: "(102)"
         },
         {
-            img: Vector2,
-            path: '/vacancies',
-            title: 'Vacancies ',
-            color: "#FF6F6F",
+            img: Agricaltural,
+            title: "Agricaltural",
+            number: '(82)'
         },
         {
-            img: Vector3,
-            path: '/careers',
-            title: 'Careers ',
-            color: "#4886FF",
+            img: Catering,
+            title: "Catering",
+            number: '(125)'
         },
         {
-            img: Vector4,
-            path: '/contact',
-            title: 'Contact',
-            color: "#009688",
+            img: Logistics,
+            title: "Logistics",
+            number: '(97)'
         },
+        {
+            img: Housekeeping,
+            title: "Housekeeping",
+            number: '(102)'
+        },
+        {
+            img: Administration,
+            title: "Administration Management",
+            number: '(102)'
+        },
+        {
+            img: Customer,
+            title: "Customer Service",
+            number: '(97)'
+        }
     ])
 
 
 
     return <div>
-        <div className="P-welcome">
+        <div className="P-finds-job">
             <div className="G-container">
-                <h1>Welcome to <span>VBK Concultancy</span></h1>
-                <p>Best Platform to Learn Everything</p>
-                <div className="P-welcome-boxes G-flex G-align-center G-justify-between">
+                <h1>Finds <span> jobs </span> in your Field</h1>
+                <div className="P-finds-job-boxes G-flex G-flex-wrap G-justify-center">
                     {box.map((elem,index)=>{
-                        return <div key={index} className="P-welcome-box G-flex G-flex-column G-center" style={{backgroundColor: elem.color}}>
-                            <img src={elem.img} alt="Logo"/>
-                            <p>{elem.title}</p>
+                        return <div key={index} className="P-finds-box G-flex G-align-center">
+                            <img src={elem.img} alt="box-logo"/>
+                            <div>
+                                <p>{elem.title}</p>
+                                <span>{elem.number}</span>
+                            </div>
                         </div>
                     })}
                 </div>
