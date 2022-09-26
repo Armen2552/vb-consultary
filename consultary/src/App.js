7544a933599179573f268a673c647366b39816f5
Armen2552 <armenhayrapetyan@gmail.com>
2022-09-26 21:59:21 +0400

    26-09

--- consultary/src/App.js
+++ consultary/src/App.js
@@ -3,7 +3,7 @@ import Map from "./components/header";
 import Main from "./pages/main-page";
 import Welcome from "./components/Section-welcome";
 import LatestNews from "./components/Section-lates-news";
-// import Footer from "./components/footer";
+import Footer from "./components/footer";
 import "./assets/icon/style.css"
 import Video from "./components/Video-cover";
 // import {Profile} from "./components/Profile";
@@ -17,7 +17,7 @@ function App() {
       <Welcome />
       <Video />
       <LatestNews />
-      {/*<Footer />*/}
+      <Footer />
       {/*<Profile />*/}
     </div>
   );
--- consultary/src/components/footer/index.jsx
+++ consultary/src/components/footer/index.jsx
@@ -1,43 +1,90 @@
 import React, {useState} from "react";
 import "./style.scss"
 import LogoFooter from "../../assets/images/logo.svg"
+import Tel from "../../assets/images/tel.png"
+import Massage from "../../assets/images/massage.png"
+import Instagram from "../../assets/images/ins.png"
+import Fb from "../../assets/images/fb.png"
+import Youtube from "../../assets/images/youtube.png"
+import Watsapp from "../../assets/images/watsapp.png"
 
 const Footer = ()=>{
 
     const[list1] = useState([
         {
-            description: "Feature Links",
-            path: "/feature-link"
-        },
-        {
-            description: "About us",
+            description: "Home",
             path: "/about-us"
         },
         {
-            description: "Vacancies",
+            description: "About Us",
             path: "/vacancies"
         },
         {
-            description: "Feature Links",
+            description: "Vacancies",
             path: "/feature-link"
         },
 
     ])
 
+    const[list2] = useState([
+        {
+            description: "Privacy",
+            path: "/privacy"
+        },
+        {
+            description: "FAQs",
+            path: "/faqs"
+        },
+        {
+            description: "Terms",
+            path: "/terms"
+        },
 
-    return <div>
-        <div className="P-main-footer">
-            <div className="G-container">
+    ])
+
+
+    return <div className="P-main-footer">
+            <div className="G-container G-flex G-justify-between">
                 <div className="P-footer-logo G-flex G-center G-flex-column">
                     <img src={LogoFooter} alt="Logo"/>
                     <p>VBK CONSULTANCY</p>
                 </div>
+                <div className="P-feature">
+                    <h2>Feature Links</h2>
+                    <ul>
+                        {list1.map((elem,index)=>{
+                            return <li key={index}>{elem.description}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="P-support">
+                    <h2>Support</h2>
+                    <ul>
+                        {list2.map((elem,index)=>{
+                            return <li key={index}>{elem.description}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="P-footer-contact">
+                    <h2>Contact Us</h2>
+                    <ul>
+                        <li><img src={Tel} alt="tel"/>+971 50 913 4617</li>
+                        <li><img src={Massage} alt="massage"/>info@vbkc.in</li>
+                        <ul className="G-flex G-justify-between">
+                            <li><img src={Instagram} alt="Instagram"/></li>
+                            <li><img src={Fb} alt="Facebook"/></li>
+                            <li><img src={Youtube} alt="Youtube"/></li>
+                            <li><img src={Watsapp} alt="Watsapp"/></li>
+                        </ul>
+                    </ul>
+                </div>
+            </div>
+            <div className="P-footer-reserved G-flex G-center">
+                <p>© VBK Consultancy 2022. All rights reserved.</p>
             </div>
         </div>
-        <div className="P-footer-reserved">
-            <p>© VBK Consultancy 2022. All rights reserved.</p>
-        </div>
-    </div>
+
+
 }
 
 export default Footer
